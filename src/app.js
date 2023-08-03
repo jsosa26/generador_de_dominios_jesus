f380d0b1c69771fac8edfab6869302560d8001e9
jsosa26 <jesus.asm26@gmail.com>
2023-08-03 18:55:22 +0000

    primeras variables colocadas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "puedes"];
+let ext = [".com", ".net", ".us", ".io"];
