35e8c5ce9c9a8d9d4a29f8193d77f9519a1cd3f7
jsosa26 <jesus.asm26@gmail.com>
2023-08-03 19:02:10 +0000

    codigo casi listo, faltan detalles

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon", "puedes"];
-let ext = [".com", ".net", ".us", ".io"];
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "puedes"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  pronoun.forEach(pronombre => {
+    adj.forEach(adjetivo => {
+      noun.forEach(sustantivo => {
+        console.log(
+          `${sustantivo == "puedes" ? `${pronombre}${adjetivo}pued.es` : ""}`
+        );
+        ext.forEach(extension => {
+          console.log(`
+        ${
+          sustantivo == "puedes" &&
+          (extension == ".com" ||
+            extension == ".net" ||
+            extension == ".us" ||
+            extension == ".io")
+            ? ""
+            : `${pronombre}${adjetivo}${sustantivo}${extension}`
+        }
+        `);
+        });
+      });
+    });
+  });
+};
