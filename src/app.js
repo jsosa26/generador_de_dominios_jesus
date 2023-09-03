989ac29f8cfc7293c8d92ea15968b929ba9f0c77
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-03 18:43:40 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -4,32 +4,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon", "puedes"];
-  let ext = [".com", ".net", ".us", ".io"];
+var pronoun = ['the', 'our'];
+var adj = ['great', 'big'];
+var noun = ['jogger', 'racoon'];
 
-  pronoun.forEach(pronombre => {
-    adj.forEach(adjetivo => {
-      noun.forEach(sustantivo => {
-        console.log(
-          `${sustantivo == "puedes" ? `${pronombre}${adjetivo}pued.es` : ""}`
-        );
-        ext.forEach(extension => {
-          console.log(`
-        ${
-          sustantivo == "puedes" &&
-          (extension == ".com" ||
-            extension == ".net" ||
-            extension == ".us" ||
-            extension == ".io")
-            ? ""
-            : `${pronombre}${adjetivo}${sustantivo}${extension}`
-        }
-        `);
-        });
-      });
-    });
-  });
-};
+var domains = [];
+
+for (var i = 0; i < pronoun.length; i++) {
+  for (var j = 0; j < adj.length; j++) {
+    for (var k = 0; k < noun.length; k++) {
+      var domain = pronoun[i] + adj[j] + noun[k] + '.com';
+      domains.push(domain);
+    }
+  }
+}
+
+console.log(domains);
